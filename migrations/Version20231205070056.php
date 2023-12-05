dd5f9d715266dc13b2848f7519d1140aea1e262e
Stefan Chonov <stefan.chonov@gmail.com>
2023-12-05 02:38:53 -0500

    Updated products page. UI and functionality.

---
+++ migrations/Version20231205070056.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231205070056 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product ADD ingredients VARCHAR(500) NOT NULL, ADD recomended_use LONGTEXT NOT NULL, ADD storage_conditions LONGTEXT NOT NULL, ADD main_characteristics LONGTEXT NOT NULL, CHANGE price price NUMERIC(7, 2) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product DROP ingredients, DROP recomended_use, DROP storage_conditions, DROP main_characteristics, CHANGE price price DOUBLE PRECISION NOT NULL');
+    }
+}
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -45,13 +45,4 @@ class ProductRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
-
-    public function findWithRelations(): array
-    {
-        return $this->createQueryBuilder('p,i')
-            ->innerJoin('products_images', 'pi', null, 'pi.product_id = p.id')
-            ->leftJoin('images', 'i', null, 'pi.image_id = i.id')
-            ->getQuery()
-            ->getArrayResult();
-    }
 }
